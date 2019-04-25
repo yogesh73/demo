d9b4ce2db1c5a395d0c5f03a967649cb15960071
ym029711 <Yogesh.mittal@nagarro.com>
2019-04-25 12:23:07 +0530

    US583824: commit5

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,20 +5,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		System.out.println("test");
-		System.out.println("test");
-		System.out.println("test");
-		System.out.println("test");
-		System.out.println("CI-1706");
-		System.out.println("\"CI-1706\"");
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println("");
-		System.out.println();
-		System.out.println();
+		
 		
 	}
 	
