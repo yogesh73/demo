633ea1d0098ed3d52c9e88f796c46b1b4b64bc5b
ym029711 <Yogesh.mittal@nagarro.com>
2019-05-06 16:33:41 +0530

    TLPI-153: Issue not imported

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,7 +4,7 @@ package com.example.demo;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("sdsfeg-1");
+		System.out.println("f-1");
 		System.out.println("wed");
 		
 		
