7cf7e10da7cf8518bc18617c74d8337ba1bc0c7a
ym029711 <Yogesh.mittal@nagarro.com>
2019-05-06 15:18:28 +0530

    US583824: Issue not imported

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,7 +5,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		System.out.println("test-1");
-		System.out.println("drjbgdh");
+		System.out.println("dfgf");
 		
 		
 	}
