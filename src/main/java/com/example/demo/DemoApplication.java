836eef40e3b7c0fd89b27a81fdad818a5743c769
ym029711 <Yogesh.mittal@nagarro.com>
2019-05-07 11:38:17 +0530

    US571402: Issue not importedgfhf

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,7 +5,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		System.out.println("fdfsef-1");
-		System.out.println("sdf");
+		System.out.println("sdfsdfd");
 		
 		
 	}
