2c01cbb6507f4afd3b7238700e40a357cf4e3d06
ym029711 <Yogesh.mittal@nagarro.com>
2019-05-07 16:12:52 +0530

    SWCDDTRAIN-39:yogesh Issue

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,7 +5,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		System.out.println("fdfsef-1");
-		System.out.println("sdfsdfd");
+		System.out.println("sds");
 		
 		
 	}
