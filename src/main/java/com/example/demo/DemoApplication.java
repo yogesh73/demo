bda2a2030749e0128887f255d895fa681b70106f
ym029711 <Yogesh.mittal@nagarro.com>
2019-05-09 14:40:04 +0530

    SWCDDTRAIN-35:yogesh Issue

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,7 +5,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		System.out.println("sdsds-1");
-		System.out.println("asdsad");
+		System.out.println("asdsadsddsf");
 		
 		
 	}
