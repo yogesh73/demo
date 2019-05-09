dcb8ef8686dc8609b66e8e1fb1283905811c0a60
ym029711 <Yogesh.mittal@nagarro.com>
2019-05-09 14:38:45 +0530

    SWCDDTRAIN-33:yogesh Issue

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,7 +4,7 @@ package com.example.demo;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("sdasdsfsdf-1");
+		System.out.println("sdsds-1");
 		System.out.println("sds");
 		
 		
