c7b03dc92422dd90decb66e8c75f13196d248e2b
ym029711 <Yogesh.mittal@nagarro.com>
2019-05-13 16:14:36 +0530

    US574683:yogesh Issue

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,7 +4,7 @@ package com.example.demo;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("sadsadas-1");
+		System.out.println("werwerew-1");
 		System.out.println("sds");
 		
 		
