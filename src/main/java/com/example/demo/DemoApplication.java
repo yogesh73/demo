5fbe3315191609375970755d5443c28f8206a9db
ym029711 <Yogesh.mittal@nagarro.com>
2019-05-13 16:09:47 +0530

    US574683:yogesh Issue

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,7 +5,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		System.out.println("sadsadas-1");
-		System.out.println("asdsaasdasddsddsf");
+		System.out.println("sds");
 		
 		
 	}
