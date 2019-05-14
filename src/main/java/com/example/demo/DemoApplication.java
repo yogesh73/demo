222a2f14855e09681a26a648c9f004d3d384e798
ym029711 <Yogesh.mittal@nagarro.com>
2019-05-14 11:50:26 +0530

    SWCDDTRAIN-41: rfrhgr

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,7 +5,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		System.out.println("sdfds-1");
-		System.out.println("sdfds");
+		System.out.println("sds");
 		
 		
 	}
