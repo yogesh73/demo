8826722452d362b17d3568b8d43fc1ab535a85b2
ym029711 <Yogesh.mittal@nagarro.com>
2019-05-14 15:40:02 +0530

    SWCDDTRAIN-20: rfrhgr

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,7 +5,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		System.out.println("sdsfdssddsfds-1");
-		System.out.println("xfxdxdfdg");
+		System.out.println("xfzcxdxdfdg");
 		
 		
 	}
