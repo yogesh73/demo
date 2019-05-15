c49f73042d6c0a5bb980a6a8d9803834216ccbe3
ym029711 <Yogesh.mittal@nagarro.com>
2019-05-15 15:42:17 +0530

    SWCDDTRAIN-43:sjdfjkds

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,7 +5,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		System.out.println("-1");
-		System.out.println("sdfdsfsdfsdsdfgdfdfsseresdgdfsefs");
+		System.out.println("sefsd");
 		
 		
 	}
