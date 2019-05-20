0892b833863d438f01767819477ac493c4baf629
ym029711 <Yogesh.mittal@nagarro.com>
2019-05-20 17:17:13 +0530

    SWCDDTRAIN-3700: ieweirfr

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,7 +4,7 @@ package com.example.demo;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		//StringBuilder str = new StringBuilder();
+		StringBuilder str = new StringBuilder();
 		//System.out.println(str.length() != 0?str.toString():0);
 		System.out.println(1/10);
 
