795ffdb71c56290308129f9824920613d441a0f0
ym029711 <Yogesh.mittal@nagarro.com>
2019-05-21 11:46:48 +0530

    SWCDDTRAIN-37: ieweirfr

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,6 +6,7 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		StringBuilder str = new StringBuilder();
 		System.out.println(str.length() != 0?str.toString():0);
+		System.out.println("erfre");
 		System.out.println(1/10);
 
 	
