dab9493a05d36a7f40c4e290763c89701d321756
ym029711 <Yogesh.mittal@nagarro.com>
2019-06-12 12:01:47 +0530

    US564864

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,9 +5,15 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		StringBuilder str = new StringBuilder();
-		System.out.println(str.length() != 0?str.toString():0);
-		System.out.println("sdfsdfasdsasdfds");
-		System.out.println(1/10);
+		//System.out.println(str.length() != 0?str.toString():0);
+		//System.out.println("sdfsdfasdsasdfds");
+		//System.out.println(1/10);
+		int[] arr= new int[10];
+	//	System.out.println(arr);
+		//System.out.println(arr.length);
+ 		//return new int[] {};
+		
+		System.out.println(String.format("job_%s.txt", 21));
 
 	
 	}
