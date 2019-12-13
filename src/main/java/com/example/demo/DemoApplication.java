b004448f2e87157a989b54bc7d5b381fa87049e9
yogesh73 <44692270+yogesh73@users.noreply.github.com>
2019-12-13 11:34:54 +0530

    change from Web

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,7 +9,7 @@ public class DemoApplication {
 		System.out.println("fsdfdsd");
 		int[] arr= new int[10];
 	//	System.out.println(arr);
-		//System.out.println(arr.length);
+		System.out.println(arr.length);
  		//return new int[] {};
 		
 		System.out.println(String.format("job_%s.txt", 21));
